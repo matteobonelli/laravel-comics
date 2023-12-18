e7498a9ddc86859bb4ab326c11b95880cdf79b9f
matteobonelli <matteobonelli08@gmail.com>
2023-12-18 16:24:27 +0100

    added comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,18 @@
 
 @section('content')
 <main>
-    <h1>Homepage</h1>
+    <div class="container">
+        <h1>Homepage</h1>
+        <div class="row">
+            @foreach ($comics as $comic)
+            <div class="col-12 col-md-2 mb-4">
+                <img src="{{$comic['thumb']}}" :alt="{{$comic['title']}}" class="mb-3">
+                <h5 class="text-uppercase">{{ $comic['series'] }}</h5>
+            </div>
+            @endforeach
+        </div>
+    </div>
+    
 </main>
 
 @endsection
